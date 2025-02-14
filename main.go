34317d91f32a3bd28881c93b7973f863695c61e0
felver-dev <felver.ngouala@gmail.com>
2025-02-14 20:41:47 +0100

    Golang Datatypes

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Affiche deux variables en les concatenant
+	var name = "Jules"
+	var firstName = "Felver"
+
+	fmt.Println("Hello", name+" "+firstName)
+
+	// LES TYPES DES VARIABLES :
+	/*
+		Un type qualifie la nature de la donnée passée à une variable.
+		Ex :
+			var age = 24
+			var prenom = "Jules"
+			var PI = 3.14
+
+			Nous avons les entiers, les chaines de caractères, les booléens (True ou false), les nombres flottants : Nombres à virgules
+
+			var isAdult = false
+
+			demander à l'utilisateur de rentrer son âge. Si son âge est strictement inférieur à 18 ans alors isAdult = true ,
+	*/
+
+	// LES ENTIERS : INTEGERS | INT
+	var age = 25
+	// var years = "ans"
+	fmt.Println("I am", age, "years old")
+
+	// Il e
+}
